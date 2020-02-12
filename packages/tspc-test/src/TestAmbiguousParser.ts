abb43b7894360e997c48276c14f38134325e5f4e
Zihan Chen (MSFT) <zihanc@microsoft.com>
2020-02-12 15:03:10 -0800

    Test amb()

--- packages/tspc-test/src/TestAmbiguousParser.ts
+++ packages/tspc-test/src/TestAmbiguousParser.ts
@@ -7,7 +7,7 @@
 import * as assert from 'assert';
 import * as parsec from 'typescript-parsec';
 import { buildLexer, Token } from 'typescript-parsec';
-import { alt, amb, apply, kright, rule, seq, str, tok } from 'typescript-parsec';
+import { alt, amb, apply, kright, lrec_sc, rule, seq, str, tok } from 'typescript-parsec';
 
 function notUndefined<T>(t: T | undefined): T {
     assert.notStrictEqual(t, undefined);
@@ -33,27 +33,59 @@ const lexer = buildLexer([
     [false, /^\s+/g, TokenKind.Space]
 ]);
 
+const term = rule<TokenKind, string>();
 const expr = rule<TokenKind, string>();
+
+term.setPattern(
+    alt(
+        apply(tok(TokenKind.Number), (t: Token<TokenKind>) => t.text),
+        apply(kright(str('+'), expr), (s: string) => `(+ ${s})`)
+    )
+);
+
 expr.setPattern(
     apply(
         amb(
-            alt(
-                apply(tok(TokenKind.Number), (t: Token<TokenKind>) => t.text),
-                apply(kright(str('+'), expr), (s: string) => `(+ ${s})`),
-                apply(seq(expr, kright(str('+'), expr)), ([s, t]: [string, string]) => `(${s} + ${t})`),
-                apply(seq(expr, expr), ([s, t]: [string, string]) => `(${s} . ${t})`)
-            )
+            lrec_sc(
+                term,
+                alt(expr, seq(str('+'), expr)),
+                (s: string, t: string | [Token<TokenKind>, string]) =>
+                    typeof t === 'string'
+                        ? `(${s} . ${t})`
+                        : `(${s} + ${t[1]})`)
         ),
         (ss: string[]) => ss.length === 1 ? ss[0] : `[${ss.join(', ')}]`
     )
 );
+
+test(`Parser: 1`, () => {
+    const firstToken = notUndefined(lexer.parse(`1`));
+    {
+        const result = succeeded(expr.parse(firstToken));
+        assert.strictEqual(result.length, 1);
+        assert.strictEqual(result[0].result, '1');
+        assert.strictEqual(result[0].firstToken, firstToken);
+        assert.strictEqual(result[0].nextToken, undefined);
+    }
+});
+
+test(`Parser: +1`, () => {
+    const firstToken = notUndefined(lexer.parse(`+1`));
+    {
+        const result = succeeded(expr.parse(firstToken));
+        assert.strictEqual(result.length, 1);
+        assert.strictEqual(result[0].result, '(+ 1)');
+        assert.strictEqual(result[0].firstToken, firstToken);
+        assert.strictEqual(result[0].nextToken, undefined);
+    }
+});
 
 test(`Parser: 1+2`, () => {
     const firstToken = notUndefined(lexer.parse(`1+2`));
     {
         const result = succeeded(expr.parse(firstToken));
         assert.strictEqual(result.length, 1);
-        assert.strictEqual(result[0].result, '[(1 + 2), (1 . (+ 2))]');
+        assert.strictEqual(result[0].result, '[(1 . (+ 2)), (1 + 2)]');
         assert.strictEqual(result[0].firstToken, firstToken);
         assert.strictEqual(result[0].nextToken, undefined);
     }
